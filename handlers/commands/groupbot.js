c6236e09353af92d56708d6d26cf81226701936b
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2022-03-29 19:43:18 +0700

    Update groupbot.js

--- handlers/commands/groupbot.js
+++ handlers/commands/groupbot.js
@@ -1,12 +1,9 @@
 let cap = `Join Group Bot untuk info ter-update
 
-• *Telegram Channel* : https://t.me/info_mg
-• *WhatsApp Group 1* : https://chat.whatsapp.com/KSupzi0Ez28AxNkViVaJfy
-• *WhatsApp Group 2* : https://chat.whatsapp.com/DJFe116CDyb26pb2pYDoy2
+• https://chat.whatsapp.com/IKcqBMcirblBbc9SqpKuPu
 `
 const buttonsDefault = [
-    { urlButton: { displayText: `🛎 Telegram Bot`, url: `https://t.me/tikdl_bot` } },
-    { urlButton: { displayText: `💈 Instagram`, url: 'https://www.instagram.com/mg.bot' } },
+    { urlButton: { displayText: `Instagram`, url: 'https://www.instagram.com/terserah_bomat' } },
 ]
 
 module.exports = {
