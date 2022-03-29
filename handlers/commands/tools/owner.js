636e8868e0454dc3af6a2ca2ddb14633ef5409cf
pumpa0 <87925130+pumpa0@users.noreply.github.com>
2022-03-29 21:17:08 +0700

    Update owner.js

--- handlers/commands/tools/owner.js
+++ handlers/commands/tools/owner.js
@@ -10,10 +10,7 @@ module.exports = {
         config.owner.map(async (v) => await client.sendContact(m.chat, v.split(S_WHATSAPP_NET)[0], package.author, m))
         await delay(2000)
         const btn = [
-            { urlButton: { displayText: `🌐 Web`, url: `https://masgimenz.my.id` } },
-            { urlButton: { displayText: `📸 Instagram`, url: `https://www.instagram.com/gimenz.id` } },
-            { urlButton: { displayText: `🐈 Github`, url: `https://github.com/Gimenz` } },
-            { urlButton: { displayText: `🎨 TikTok`, url: `https://www.tiktok.com/@gh0stp0w3r` } },
+            { urlButton: { displayText: `Instagram`, url: `https://www.instagram.com/terserah_bomat` } },
         ]
         client.sendMessage(m.chat, { text: `Social Media`, footer: global.footer, templateButtons: btn }, { quoted: m })
     }
